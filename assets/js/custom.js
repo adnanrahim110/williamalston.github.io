f8f9d02a78c087a33d9f8e3f16e50c8f0dd3f150
adnan rahim <adnankaka.786110@gmail.com>
2024-07-08 21:26:36 +0500

    "Updated codebase: removed settings.json, added popup functionality, modified CSS and JS files, updated HTML structure, and changed image files."

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	"use strict";
 
+	//  Popup on load
+	var popup = document.getElementById("sale-popup");
+	var span = document.getElementsByClassName("popup-close")[0];
+	window.onload = function () {
+		popup.style.display = "flex";
+	}
+	span.onclick = function () {
+		popup.style.display = "none";
+	}
+	window.onclick = function (event) {
+		if (event.target == popup) {
+			popup.style.display = "none";
+		}
+	}
+
+	$('.popup-free').on('click', function (event) {
+        event.preventDefault(); // Prevent default action of the link
+        popup.style.display = "flex"; // Show the popup
+    });
+
+
 	//  Scroll To Top 
-	$(window).on('scroll',function () {
+	$(window).on('scroll', function () {
 		if ($(this).scrollTop() > 300) {
 			$('.return-to-top').fadeIn();
 		} else {
 			$('.return-to-top').fadeOut();
 		}
 	});
-	$('.return-to-top').on('click',function(){
-			$('html, body').animate({
+	$('.return-to-top').on('click', function () {
+		$('html, body').animate({
 			scrollTop: 0
 		}, 1500);
 		return false;
@@ -22,26 +43,26 @@ $(document).ready(function(){
 
 	// Loop through the buttons and add the active class to the current/clicked button
 	for (var i = 0; i < btns.length; i++) {
-	btns[i].addEventListener("click", function() {
-		var current = document.getElementsByClassName("active");
-		current[0].className = current[0].className.replace(" active", "");
-		this.className += " active";
-	});
+		btns[i].addEventListener("click", function () {
+			var current = document.getElementsByClassName("active");
+			current[0].className = current[0].className.replace(" active", "");
+			this.className += " active";
+		});
 	}
 
-	window.addEventListener('load', function() {
+	window.addEventListener('load', function () {
 		// Fetch all the forms we want to apply custom Bootstrap validation styles to
 		var forms = document.getElementsByClassName('needs-validation');
 		// Loop over them and prevent submission
-		var validation = Array.prototype.filter.call(forms, function(form) {
-		  form.addEventListener('submit', function(event) {
-			if (form.checkValidity() === false) {
-			  event.preventDefault();
-			  event.stopPropagation();
-			}
-			form.classList.add('was-validated');
-		  }, false);
+		var validation = Array.prototype.filter.call(forms, function (form) {
+			form.addEventListener('submit', function (event) {
+				if (form.checkValidity() === false) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				form.classList.add('was-validated');
+			}, false);
 		});
-	  }, false);
+	}, false);
 
 });
